32d77cd1e505cf443bd8687ae79140d88ed6a894
q103413 <q103413@gmail.com>
2017-06-06 18:35:54 +0800

    t

---
+++ multithreaded/src/multithreaded/NewThread.java
@@ -0,0 +1,10 @@
+package multithreaded;
+
+class NewThread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ multithreaded/src/multithreaded/NewThread1.java
@@ -0,0 +1,10 @@
+package multithreaded;
+
+public class NewThread1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
